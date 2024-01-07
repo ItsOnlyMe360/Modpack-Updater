c7d8e57328abcc0b66ccaf5d9303a885e4d1f1df
Enzo <126713125+ItsOnlyMe360@users.noreply.github.com>
2024-01-06 22:41:47 -0300

    Update KKSMPupdater.py
    Botão de update atualizado.
    Informações dos passos de download e extração mais detalhados.

--- updater/KKSMPupdater.py
+++ updater/KKSMPupdater.py
@@ -23,40 +23,72 @@ def resource_path(relative_path):
 def baixar_e_descompactar():
     url_raw = "https://raw.githubusercontent.com/ItsOnlyMe360/KKSMP/main/redirect.txt"
 
-    response = requests.get(url_raw)
-    raw_text = response.text
+    try:
+        label_status.config(text="Baixando atualização...", bg='#1f1f1f')
+        root.update()
 
-    url_file = raw_text.strip()
+        response = requests.get(url_raw)
+        response.raise_for_status()
+        raw_text = response.text.strip()
 
-    response_file = requests.get(url_file)
-    zip_filename = "mods.zip"
+        url_file = raw_text
 
-    with open(zip_filename, "wb") as zip_file:
-        zip_file.write(response_file.content)
+        response_file = requests.get(url_file, stream=True)
+        response_file.raise_for_status()
 
-    with zipfile.ZipFile(zip_filename, "r") as zip_ref:
-        total_files = len(zip_ref.namelist())
-        extracted_files = 0
+        zip_filename = "mods.zip"
+        total_size = int(response_file.headers.get('content-length', 0))
+        block_size = 1024
+        downloaded_size = 0
+        last_percent = 0
 
-        for file in zip_ref.namelist():
-            zip_ref.extract(file, "temp")
-            extracted_files += 1
+        with open(zip_filename, 'wb') as zip_file:
+            for data in response_file.iter_content(block_size):
+                zip_file.write(data)
+                downloaded_size += len(data)
+                percent = int((downloaded_size / total_size) * 100)
 
-    minecraft_path = os.path.join(os.getcwd(), ".minecraft")
+                if percent > last_percent:
+                    label_status.config(text=f"Baixando atualização... {percent}%", bg='#1f1f1f')
+                    root.update()
+                    last_percent = percent
 
-    if os.path.exists(minecraft_path):
-        for folder in ["mods", "resourcepacks", "shaderpacks", "config", "MenuLayout"]:
-            folder_path = os.path.join(minecraft_path, folder)
-            if os.path.exists(folder_path):
-                shutil.rmtree(folder_path)
+        label_status.config(text="Descompactando arquivos...", bg='#1f1f1f')
+        root.update()
 
-        merge_folders("temp", minecraft_path)
-    else:
-        os.rename("temp", minecraft_path)
+        with zipfile.ZipFile(zip_filename, "r") as zip_ref:
+            total_files = len(zip_ref.namelist())
+            extracted_files = 0
+            last_percent = 0
 
-    os.remove(zip_filename)
+            for file in zip_ref.namelist():
+                zip_ref.extract(file, "temp")
+                extracted_files += 1
+                percent = int((extracted_files / total_files) * 100)
 
-    label_status.config(text="Atualização concluída!", foreground="white", background="#1F1F1F")
+                if percent > last_percent:
+                    label_status.config(text=f"Descompactando arquivos... {percent}%", bg='#1f1f1f')
+                    root.update()
+                    last_percent = percent
+
+        minecraft_path = os.path.join(os.getcwd(), ".minecraft")
+
+        if os.path.exists(minecraft_path):
+            for folder in ["mods", "resourcepacks", "shaderpacks", "config", "MenuLayout", "kubejs"]:
+                folder_path = os.path.join(minecraft_path, folder)
+                if os.path.exists(folder_path):
+                    shutil.rmtree(folder_path)
+
+            merge_folders("temp", minecraft_path)
+        else:
+            os.rename("temp", minecraft_path)
+
+        os.remove(zip_filename)
+
+        label_status.config(text="Atualização concluída!", bg='#1f1f1f')
+
+    except requests.exceptions.RequestException as e:
+        label_status.config(text=f"Erro durante a atualização: {str(e)}", bg='##1f1f1f')
 
 def merge_folders(src, dest):
     for item in os.listdir(src):
@@ -67,24 +99,40 @@ def merge_folders(src, dest):
         else:
             shutil.copy(s, d)
 
+def on_enter(event):
+    button_label.config(image=button_hover)
+
+def on_leave(event):
+    button_label.config(image=button_image)
+
 root = tk.Tk()
 root.title("KKSMP Updater")
 root.resizable(False, False)
 root.geometry("854x480")
+
+icon_path = resource_path("icon2.ico")
+root.iconbitmap(default=icon_path)
 
 bg_image = Image.open(resource_path("bg.png"))
 bg_photo = ImageTk.PhotoImage(bg_image)
 bg_label = tk.Label(root, image=bg_photo)
 bg_label.place(relwidth=1, relheight=1)
 
-style = ttk.Style()
-style.configure('TButton', padding=6, relief="flat", font=('Helvetica', 12))
-style.configure('TLabel', font=('Helvetica', 12))
+# Carregar imagens do botão
+button_image = Image.open(resource_path("botao.png"))
+button_hover_image = Image.open(resource_path("botaohover.png"))
 
-button_baixar = ttk.Button(root, text="Atualizar", command=baixar_e_descompactar, style='TButton')
-button_baixar.place(relx=0.5, rely=0.6, anchor=tk.CENTER)
+# Configurar eventos de rato para o botão
+button_image = ImageTk.PhotoImage(button_image)
+button_hover = ImageTk.PhotoImage(button_hover_image)
+button_label = tk.Label(root, image=button_image, bd=0, relief="flat", cursor="hand2")
+button_label.place(relx=0.5, rely=0.6, anchor=tk.CENTER)
 
-label_status = ttk.Label(root, text="", foreground="black", style='TLabel')
-label_status.place(relx=0.5, rely=0.7, anchor=tk.CENTER)
+button_label.bind("<Enter>", on_enter)
+button_label.bind("<Leave>", on_leave)
+button_label.bind("<Button-1>", lambda event: baixar_e_descompactar())
+
+label_status = tk.Label(root, text="", foreground="white", font=('Helvetica', 10), anchor='center', bg='#1f1f1f', padx=10, pady=5)
+label_status.place(relx=0.5, rely=0.8, anchor=tk.CENTER)
 
 root.mainloop()
